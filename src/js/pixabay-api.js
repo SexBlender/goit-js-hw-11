93f1ecc1472146dd06f85542469149e9c174f127
AntonD <adikhtenko@gmail.com>
2026-06-24 15:36:46 +0300

    hw-11

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,1 +1,18 @@
-console.log(123);
+import axios from 'axios';
+
+const API_KEY = '55520229-6b38dd18315a985816c1e4a23';
+
+const serverApi = axios.create({
+  baseURL: 'https://pixabay.com/api/',
+});
+
+export function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+  return serverApi.get('', { params }).then(res => res.data);
+}
--- src/main.js
+++ src/main.js
@@ -1,2 +1,58 @@
 import './js/pixabay-api';
 import './js/render-functions';
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const query = formData.get('search-text');
+
+  if (!query) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Please enter a search term!',
+    });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(query)
+    .then(data => {
+      const images = data.hits || [];
+
+      if (!images.length) {
+        iziToast.error({
+          title: 'No results',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+        return;
+      }
+      createGallery(images);
+    })
+    .catch(err => {
+      iziToast.error({
+        title: 'Error',
+        message: 'Something went wrong. Try again later.',
+      });
+    })
+    .finally(() => {
+      hideLoader();
+    });
+  form.reset();
+}
